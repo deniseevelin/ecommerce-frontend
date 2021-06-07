d9edffd3be043c4c119e51a918a7995c6fc269ff
DENISE EVELIN <deniseevelynk3@hotmail.com>
2021-06-07 17:30:32 -0300

    a

--- controllers/products-controller.js
+++ controllers/products-controller.js
@@ -4,7 +4,6 @@ const productsController = {
   list: async (req, res, next) => {
     try {
       let products = await repository.list();
-      return res.status(400).send({ error: products });
       res.render("products/allproducts.ejs", {products: products.data });
     } catch (err) {
       return res.status(400).send({ error: "Error finding products!" });
